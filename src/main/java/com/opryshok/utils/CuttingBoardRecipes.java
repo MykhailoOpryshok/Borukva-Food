26fbef57b0399ffe128af65650156538557740b0
Zefir <stickulzon@gmail.com>
2024-11-08 22:54:07 +0100

    - Updated harvest sickle texture - Bread have 4 slices now

--- src/main/java/com/opryshok/utils/CuttingBoardRecipes.java
+++ src/main/java/com/opryshok/utils/CuttingBoardRecipes.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 public class CuttingBoardRecipes {
     public static final HashMap<Item, ItemStack> RECIPES = new HashMap<>();
     static {
-        put(Items.BREAD, new ItemStack(ModItems.BREAD_SLICE, 2));
+        put(Items.BREAD, new ItemStack(ModItems.BREAD_SLICE, 4));
         put(Items.BEEF, new ItemStack(ModItems.BEEF_SLICES, 2));
         put(ModItems.CHILLI_PEPPER, new ItemStack(ModItems.HOT_SPICE, 1));
         put(ModItems.TOMATO, new ItemStack(ModItems.TOMATO_SLICES, 2));
