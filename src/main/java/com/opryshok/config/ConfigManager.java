37c26166d3139f1b4b20c232bcbc616daeaf5edf
Stikulzon <stickulzon@gmail.com>
2025-01-23 15:58:25 +0100

    Added config validation

--- src/main/java/com/opryshok/config/ConfigManager.java
+++ src/main/java/com/opryshok/config/ConfigManager.java
@@ -1,7 +1,6 @@
 package com.opryshok.config;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
 import com.opryshok.BorukvaFood;
 import net.fabricmc.loader.api.FabricLoader;
 
@@ -9,25 +8,42 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Path;
 
 public class ConfigManager {
     private static final File CONFIG_FILE = FabricLoader.getInstance().getConfigDir().resolve("borukva-food.json").toFile();
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
     public static ModConfig load() {
-        try (FileReader reader = new FileReader(CONFIG_FILE)) {
-            return GSON.fromJson(reader, ModConfig.class);
-        } catch (IOException e) {
-            // If the file doesn't exist or there's an error, create a new config with defaults
-            ModConfig config = new ModConfig();
-            save(config);
+        ModConfig config = new ModConfig(); // Start with default config
+        if (!CONFIG_FILE.exists()) {
+            save(config); // Save default config
             return config;
         }
+
+        try (FileReader reader = new FileReader(CONFIG_FILE)) {
+            JsonObject jsonConfig =  JsonParser.parseReader(reader).getAsJsonObject();
+            config =  GSON.fromJson(jsonConfig, ModConfig.class);
+            JsonObject defaultJsonConfig = GSON.toJsonTree(new ModConfig()).getAsJsonObject();
+            for (var entry : defaultJsonConfig.entrySet()) {
+                if (!jsonConfig.has(entry.getKey())) {
+                    jsonConfig.add(entry.getKey(),entry.getValue());
+                }
+            }
+
+            config = GSON.fromJson(jsonConfig, ModConfig.class);
+
+            save(config);
+
+
+        } catch (IOException e) {
+            // If there's an error (could be parsing), create a new config with defaults and save
+            BorukvaFood.LOGGER.error("Error loading config, using default: {}", e.getMessage());
+            save(config);
+        }
+        return config;
     }
 
     public static void save(ModConfig config) {
-
         try (FileWriter writer = new FileWriter(CONFIG_FILE)) {
             GSON.toJson(config, writer);
         } catch (IOException e) {
